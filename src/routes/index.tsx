aa820ec8296e400435235e47e886be37e421420d
Rony cb <mylovepre16@gmail.com>
2020-06-24 15:48:36 -0500

    update src routes for gridmall option

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,27 +1,30 @@
-/**
- *  @version 1.0.1
- *  @author Rony cb - Mrvalem
- *  @description Manejo de todas las rutas de la aplicación
- * 	@process 3
- */
-
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 /**
  * @description Incorporación de todas las rutas
  */
-import PageIndex from "../xample/Home";
+import { HomeGirdMall } from "../zviewsgridmall";
 import Privacy from "../xample/Privacy";
 import Terms from "../xample/Terms";
 import ThemeTwo from "../xample/ThemeTwo";
+
+/**
+ * @version 1.0.1
+ * @author Rony cb - Mrvalem
+ * @description Manejo de todas las rutas de la aplicación
+ * @process 3
+ * @warning [!] por favor reemplace estas rutas con example o cree sus propias vistas para
+ * implementarlo.
+ * @example import {Home, ...etc} from '../xample/
+ */
 
 const Routes = () => (
   <BrowserRouter>
     <div className="flex main">
       <div className="column full">
         <Switch>
-          <Route path="/" exact component={PageIndex} />
+          <Route path="/" exact component={HomeGirdMall} />
           <Route path="/privacy" exact component={Privacy} />
           <Route path="/terms" exact component={Terms} />
           <Route path="/themetwo" exact component={ThemeTwo} />
