46ccf8fc8f85e36e7315fb8eae6e6a06602cc5f1
Rony cb <mylovepre16@gmail.com>
2020-06-28 15:15:53 -0500

    creating funcion connect for componentents.

--- src/context/connect.tsx
+++ src/context/connect.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { StoreContext } from "./StoreContext";
+import { state } from "./Reducer";
 
 /**
  * @version 1.0.0
@@ -8,9 +9,35 @@ import { StoreContext } from "./StoreContext";
  * @description este componente nos permitira realizar la conexcion de estados generales para
  * nuestras paginas o componentes
  */
-const connect = (Component: React.SFC<any>) => {
-  const { state, actions } = React.useContext(StoreContext);
-  return <Component state={state} actions={actions} />;
+
+export interface WithRootProps {
+  /**
+   * Recivimos los estados del Reducer
+   */
+  state: state;
+  /**
+   * Establecemos las accion para nuestro componentes
+   */
+  actions: any;
+  /**
+   * Permitimos props personalisados para nuectro Component
+   */
+  [key: string]: any;
+}
+
+const connect = (Component: React.SFC<WithRootProps>) => {
+  /**
+   * @param props Contiene el histirial de rutas y los pasamos a nuestro componente.
+   * @description Retornamos un componente SFC para no tener confictos con React.Router
+   */
+  const Connect: React.SFC<any> = (props) => {
+    /**
+     * recuperamos los estados y actions de nuestro Store Context
+     */
+    const { state, actions } = React.useContext(StoreContext);
+    return <Component state={state} actions={actions} {...props} />;
+  };
+  return Connect;
 };
 
 export default connect;
