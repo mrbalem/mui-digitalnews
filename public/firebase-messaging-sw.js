dde1851a39b67ac5a2a4420a7c56abaceb361890
Rony cb <mylovepre16@gmail.com>
2020-07-06 18:42:56 -0500

    removing config

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,14 +1,6 @@
 importScripts("https://www.gstatic.com/firebasejs/5.9.4/firebase-app.js");
 importScripts("https://www.gstatic.com/firebasejs/5.9.4/firebase-messaging.js");
 firebase.initializeApp({
-  apiKey: "AIzaSyA0KSuEXMUASdslhEopeL90NcBuU35opFM",
-  authDomain: "ink-grid.firebaseapp.com",
-  databaseURL: "https://ink-grid.firebaseio.com",
-  projectId: "ink-grid",
-  storageBucket: "ink-grid.appspot.com",
-  messagingSenderId: "665911529629",
-  appId: "1:665911529629:web:a2e61ae4df737c4dbe0474",
-  measurementId: "G-RRL0S08ZGF",
   // Project Settings => Add Firebase to your web app
   // messagingSenderId: "1062407524656",
 });
