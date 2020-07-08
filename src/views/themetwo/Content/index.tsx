b0ccbe8eb07aad1f95571977becbad7ed29108d2
Rony cb <mylovepre16@gmail.com>
2020-07-08 10:14:31 -0500

    update Content theme two

--- src/views/themetwo/Content/index.tsx
+++ src/views/themetwo/Content/index.tsx
@@ -1,21 +1,21 @@
 import React from "react";
-import AppBar from "@material-ui/core/AppBar";
-import Toolbar from "@material-ui/core/Toolbar";
-import Typography from "@material-ui/core/Typography";
+// import AppBar from "@material-ui/core/AppBar";
+// import Toolbar from "@material-ui/core/Toolbar";
+// import Typography from "@material-ui/core/Typography";
 import Paper from "@material-ui/core/Paper";
-import Grid from "@material-ui/core/Grid";
-import Button from "@material-ui/core/Button";
-import TextField from "@material-ui/core/TextField";
-import Tooltip from "@material-ui/core/Tooltip";
-import IconButton from "@material-ui/core/IconButton";
+// import Grid from "@material-ui/core/Grid";
+// import Button from "@material-ui/core/Button";
+// import TextField from "@material-ui/core/TextField";
+// import Tooltip from "@material-ui/core/Tooltip";
+// import IconButton from "@material-ui/core/IconButton";
 import {
   createStyles,
   Theme,
   withStyles,
   WithStyles,
 } from "@material-ui/core/styles";
-import SearchIcon from "@material-ui/icons/Search";
-import RefreshIcon from "@material-ui/icons/Refresh";
+// import SearchIcon from "@material-ui/icons/Search";
+// import RefreshIcon from "@material-ui/icons/Refresh";
 
 const styles = (theme: Theme) =>
   createStyles({
@@ -41,14 +41,22 @@ const styles = (theme: Theme) =>
     },
   });
 
-export interface ContentProps extends WithStyles<typeof styles> {}
+export interface ContentProps extends WithStyles<typeof styles> {
+  children: React.ReactNode;
+}
 
+/**
+ * @author Ronycb
+ * @version 1.0.0
+ * @param props
+ */
 function Content(props: ContentProps) {
-  const { classes } = props;
+  const { classes, children } = props;
 
   return (
     <Paper className={classes.paper}>
-      <AppBar
+      {children}
+      {/* <AppBar
         className={classes.searchBar}
         position="static"
         color="default"
@@ -90,7 +98,7 @@ function Content(props: ContentProps) {
         <Typography color="textSecondary" align="center">
           No users for this project yet
         </Typography>
-      </div>
+      </div> */}
     </Paper>
   );
 }
