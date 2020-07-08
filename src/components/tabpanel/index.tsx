a43d8bf62c9a7f78e6c0bb7a5d18d38f0654c789
Rony cb <mylovepre16@gmail.com>
2020-07-08 10:08:29 -0500

    adding tappanel component

---
+++ src/components/tabpanel/index.tsx
@@ -0,0 +1,28 @@
+import * as React from "react";
+
+export interface TabPanelProps {
+  index: any;
+  value: any;
+  [key: string]: any;
+}
+
+/**
+ * @version 1.0.0
+ * @author Rony CB
+ */
+const TabPanel: React.SFC<TabPanelProps> = (props) => {
+  const { children, value, index, ...other } = props;
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && <div>{children} </div>}
+    </div>
+  );
+};
+
+export default TabPanel;
