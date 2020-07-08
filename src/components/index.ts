6b9f4ad4f311d8165431d2c119cd901ed523c381
Rony cb <mylovepre16@gmail.com>
2020-07-08 10:07:00 -0500

    export table componente

--- src/components/index.ts
+++ src/components/index.ts
@@ -9,4 +9,5 @@ export { default as MuiSnackbar } from "./snackbar";
 export { default as MuiButton } from "./button";
 export { default as MuiTextField } from "./textfield";
 export { default as MuiPaper } from "./paper";
+export { default as MuTable } from "./table";
 export { toolbarStyles };
