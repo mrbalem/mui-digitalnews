b5d597d99cc40370d4b5ba6cbdeadfb58ab9cab1
Rony cb <mylovepre16@gmail.com>
2020-07-18 17:54:29 -0500

    adding esqueleton to loading products

--- src/views/ProductCategories/index.tsx
+++ src/views/ProductCategories/index.tsx
@@ -1,11 +1,11 @@
 import * as React from "react";
 import { Container, ButtonBase } from "@material-ui/core";
-import { MuiTypography } from "../../components";
+import { MuiTypography, MuiEsqueleton } from "../../components";
 import { useProductCategories } from "../../styles";
 
 export interface ProductCategoriesProps {
   title: string;
-  images: Array<{ title: string; url: string; width: string }>;
+  images: Array<{ uid: string; name: string; url: string; width: string }>;
 }
 
 /**
@@ -22,33 +22,35 @@ const ProductCategories: React.SFC<ProductCategoriesProps> = (props) => {
         {title}
       </MuiTypography>
       <div className={classes.images}>
-        {images.map((image) => (
-          <ButtonBase
-            key={image.title}
-            className={classes.imageWrapper}
-            style={{
-              width: image.width,
-            }}
-          >
-            <div
-              className={classes.imageSrc}
+        {!images && <MuiEsqueleton quantity={6} type="card" />}
+        {images &&
+          images.map((image) => (
+            <ButtonBase
+              key={image.uid}
+              className={classes.imageWrapper}
               style={{
-                backgroundImage: `url(${image.url})`,
+                width: image.width,
               }}
-            />
-            <div className={classes.imageBackdrop} />
-            <div className={classes.imageButton}>
-              <MuiTypography
-                variant="h6"
-                color="inherit"
-                className={classes.imageTitle}
-              >
-                {image.title}
-                <div className={classes.imageMarked} />
-              </MuiTypography>
-            </div>
-          </ButtonBase>
-        ))}
+            >
+              <div
+                className={classes.imageSrc}
+                style={{
+                  backgroundImage: `url(${image.url})`,
+                }}
+              />
+              <div className={classes.imageBackdrop} />
+              <div className={classes.imageButton}>
+                <MuiTypography
+                  variant="h6"
+                  color="inherit"
+                  className={classes.imageTitle}
+                >
+                  {image.name}
+                  <div className={classes.imageMarked} />
+                </MuiTypography>
+              </div>
+            </ButtonBase>
+          ))}
       </div>
     </Container>
   );
