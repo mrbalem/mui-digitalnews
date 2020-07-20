60ff2b65e4b7bbee58ec1225cf62b213eb8b0072
Rony cb <mylovepre16@gmail.com>
2020-07-19 22:21:41 -0500

    remove upercase

--- src/views/themetwo/AppAppBar/index.tsx
+++ src/views/themetwo/AppAppBar/index.tsx
@@ -103,7 +103,7 @@ function Header(props: HeaderProps) {
           <Grid container alignItems="center" spacing={1}>
             <Grid item xs>
               <Typography color="inherit" variant="h5" component="h1">
-                {getParameter("page").toLocaleUpperCase()}
+                {getParameter("page") || "Title"}
               </Typography>
             </Grid>
             <Grid item>
