fd23fa6e5acc014f9459a6e00b03c5d65a54fa3e
Rony cb <mylovepre16@gmail.com>
2020-07-21 00:16:41 -0500

    adding generate random number

--- src/utils/other/index.ts
+++ src/utils/other/index.ts
@@ -7,5 +7,9 @@ export const formatPrice = (x: number, currency: string) => {
   }
 };
 
+export function generateRandomInteger(min: number, max: number) {
+  return Math.floor(min + Math.random() * (max + 1 - min));
+}
+
 export const createRangeArray = (start: number, stop: number, step: number) =>
   Array.from({ length: (stop - start) / step + 1 }, (_, i) => start + i * step);
